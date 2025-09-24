0596178b0139e53f049b3807d2a88c98570d5269
Akash Welgama <shirmilwelgama@gmail.com>
2025-09-25 04:36:52 +0530

    creating directive

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,12 +1,13 @@
-import {AfterViewInit, Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {AfterViewInit, Directive, ElementRef, Host, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
 })
 export class HoverDirective implements OnInit{
 
-  color: string = 'rgba(169,169,236,0.79)';
+  @Input() color: string = 'rgba(169,169,236,0.79)';
   outlineColor: string = 'rgb(251,113,0)';
+  mouseHourOutlineColor: string = 'rgb(132,255,118)';
 
   constructor(
     private elementRef: ElementRef,
@@ -20,5 +21,13 @@ export class HoverDirective implements OnInit{
       this.renderer.setStyle(this.elementRef.nativeElement, 'border', '2px solid');
       this.renderer.setStyle(this.elementRef.nativeElement, 'border-color', this.outlineColor);
     }
+
+    @HostListener('mouseenter') onMouseEnter(){ //Mind the "onMouseEnter" is a custom function name and it is the decoration that listens
+    this.renderer.setStyle(this.elementRef.nativeElement, 'border-color', this.mouseHourOutlineColor);
+    }
+
+  @HostListener('mouseleave') onMouseLeave(){ //Mind the "onMouseLeave" is a custom function name and it is the decoration that listens
+    this.renderer.setStyle(this.elementRef.nativeElement, 'border-color', this.outlineColor);
+  }
 
 }
